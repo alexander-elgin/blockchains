0d87b27a967fc7b6b286ff0e3739b16fb03e6c3f
Alexander Elgin <sascha.elgin@gmail.com>
2026-01-30 19:45:29 +0600

    sign TRX tx

--- src/blockchains/trx/tx/index.ts
+++ src/blockchains/trx/tx/index.ts
@@ -3,15 +3,11 @@ import { TronWeb } from 'tronweb';
 import Tx from '../../../tx.js';
 
 export default class TronTxHandler {
-  private readonly privateKey: string;
   private readonly tronWeb: TronWeb;
 
-  constructor(explorerUrl: string, privateKey: string) {
-    this.privateKey = privateKey;
-
+  constructor(explorerUrl: string) {
     this.tronWeb = new TronWeb({
       fullHost: explorerUrl,
-      privateKey: privateKey,
     });
   }
 
@@ -19,5 +15,10 @@ export default class TronTxHandler {
   async createTx(tx: Tx): Promise<any> {
     this.tronWeb.setAddress(tx.from);
     return await this.tronWeb.transactionBuilder.sendTrx(tx.to, Number(tx.amount), tx.from);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  async signTx(unsignedTransaction: any, privateKey: string): Promise<any> {
+    return await this.tronWeb.trx.sign(unsignedTransaction, privateKey);
   }
 }
