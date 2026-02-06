1267ae24b5a945e75e8aa22d0dd78e806d1b3e4d
Alexander Elgin <sascha.elgin@gmail.com>
2026-02-06 17:09:29 +0600

    create ETH token tx

---
+++ src/blockchains/eth/tx-sender/index.ts
@@ -0,0 +1,24 @@
+import { ethers } from 'ethers';
+
+import type Tx from '../../../tx.js';
+import type { ContractInfo } from '../../../abstract/types.js';
+
+export default class EthTxSender {
+    private readonly wallet: ethers.Wallet;
+
+    constructor(RPC: string, privateKey: string) {
+        this.wallet = new ethers.Wallet(privateKey, new ethers.JsonRpcProvider(RPC));
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    async createTx(tx: Tx, contract: ContractInfo): Promise<any> {
+        const token = new ethers.Contract(contract.address, [
+            "function transfer(address to, uint256 amount) returns (bool)"
+        ], this.wallet);
+
+        const ethTx = await token?.transfer?.(tx.to, ethers.parseUnits(tx.amount.toString(), 0));
+        await ethTx.wait();
+
+        return ethTx;
+    }
+}
