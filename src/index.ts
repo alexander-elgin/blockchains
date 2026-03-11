0d616dd8a4c3776f382f2d9eaac0ecf1292c2e26
Alexander Elgin <sascha.elgin@gmail.com>
2026-03-11 17:09:33 +0600

    export tx list types

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,6 @@ export { toNumberByDecimals } from './utils/conversion.js';
 
 export { Sorting } from './types.js';
 export type { ContractInfo, Pagination, TxListOptions } from './types.js';
+export type * from './abstract/types.js';
 export type * from './abstract/blockchain.js';
 export type {default as TxSearcher} from './abstract/blockchain.js';
