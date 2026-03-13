537c56db204af34987d21a8f8e684ce94ecaed9c
Alexander Elgin <sascha.elgin@gmail.com>
2026-03-13 09:26:11 +0600

    release 1.3.4: fix the ETH txs list pagination

--- dist/blockchains/eth/token-tx-list-explorer/parse-pagination.d.ts
+++ dist/blockchains/eth/token-tx-list-explorer/parse-pagination.d.ts
@@ -1,3 +1,4 @@
+import type { EtherscanResponse } from './types.js';
 import type { Pagination } from '../../../types.js';
-export default function parsePagination({ limit, page }: Pagination): Pagination;
+export default function parsePagination({ limit, page }: Pagination, res: EtherscanResponse): Pagination;
 //# sourceMappingURL=parse-pagination.d.ts.map
--- dist/blockchains/eth/token-tx-list-explorer/parse-pagination.js
+++ dist/blockchains/eth/token-tx-list-explorer/parse-pagination.js
@@ -1,4 +1,4 @@
-export default function parsePagination({ limit, page = 1 }) {
-    return { limit, page: page + 1 };
+export default function parsePagination({ limit, page = 1 }, res) {
+    return { limit, page: page + (res.result.length < limit ? 0 : 1) };
 }
 //# sourceMappingURL=parse-pagination.js.map
